71a306aa35a661f0f7a001d27b6f59e8e400e6fc
Gianpiero <Gianpiero2012>
2013-07-26 11:45:43 +0200

    problem category when creating new ticket solved

--- Controller/TicketController.php
+++ Controller/TicketController.php
@@ -2,7 +2,6 @@
 
 namespace Liuggio\HelpDeskBundle\Controller;
 
-use Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
