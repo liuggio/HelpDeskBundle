6b7ed7e7b147c5a13f145f7237a386086e061103
danieledangeli <dangeli88.daniele@gmail.com>
2013-10-16 15:51:31 +0200

    rollback

--- Controller/CommentOperatorController.php
+++ Controller/CommentOperatorController.php
@@ -14,6 +14,54 @@ use Liuggio\HelpDeskBundle\Exception;
  */
 class CommentOperatorController extends Controller
 {
+    /**
+     * Creates a new Comment entity.
+     *
+     */
+    public function createAction($state = \Liuggio\HelpDeskBundle\Model\TicketState::STATE_REPLIED)
+    {
+        //Retrive the User from the Session
+        $user = $this->get('security.context')->getToken()->getUser();
 
+        $entity = new Comment();
+        $request = $this->getRequest();
+
+
+        $form = $this->createForm(new CommentType(), $entity);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $comment = $form->getData();
+            $form = $this->getRequest()->get('liuggio_HelpDeskBundle_commenttype');
+            $ticket_id = $form['ticket'];
+            $ticket = $this->getDoctrine()->getManager()->getRepository('LiuggioHelpDeskBundle:Ticket')->find($ticket_id);
+            if (!$ticket) {
+                throw $this->createNotFoundException('Unable to find Ticket entity.');
+            }
+            $ticketState = $this->getDoctrine()->getManager()->getRepository('\Liuggio\HelpDeskBundle\Entity\TicketState')
+                ->findOneByCode($state);
+
+            if (!$ticketState) {
+                throw new Exception(sprintf('Ticket State Not Found looking for "%s"', $ticketState));
+            }
+            //Set the createdBy user
+            $ticket->setState($ticketState);
+            $this->getDoctrine()->getManager()->persist($ticket);
+
+            $comment->setTicket($ticket);
+            $comment->setCreatedBy($user);
+            $this->getDoctrine()->getManager()->persist($comment);
+
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirect($this->generateUrl('ticket_show_admin', array('id' => $ticket_id)));
+
+        }
+
+        return $this->render('LiuggioHelpDeskBundle:CommentAdmin:new.html.twig', array(
+            'entity' => $entity,
+            'form' => $form->createView()
+        ));
+    }
 
 }
